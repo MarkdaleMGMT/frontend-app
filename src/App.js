1509c4928ccf124ef67f0ee9cab7ea647218d8e3
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-19 23:16:11 -0400

    added validation using redux-form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import { SignIn, SignUp, ForgotPassword, Dashboard } from './pages';
-import './App.scss';
+import { SignIn, ForgotPassword, Dashboard } from "./pages";
+import SignUp from "./pages/SignUp/Signup2";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-        <Switch>
-          <Route path="/signin" component={SignIn}/>
-          <Route path="/signup" component={SignUp}/>
-          <Route path="/forgotpassword" component={ForgotPassword}/>
-          <Route path="/dashboard" component={Dashboard}/>
-          <Route render={ props => <Redirect to={{ pathname: '/signin', state: { from: props.location } }} /> } />
-        </Switch>
+      <Switch>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/forgotpassword" component={ForgotPassword} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route
+          render={props => (
+            <Redirect
+              to={{ pathname: "/signin", state: { from: props.location } }}
+            />
+          )}
+        />
+      </Switch>
     </div>
   );
 }
