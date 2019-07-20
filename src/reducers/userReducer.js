a7b18d00f1409e347c28e11a8a740705c6109373
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-20 00:42:49 -0400

    added SET_USER, SET_ERROS, CLEAR_ERRORS to the userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,15 @@
-import { TOKEN, LOGOUT } from "../actions/types";
+import {
+  TOKEN,
+  LOGOUT,
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS
+} from "../actions/types";
 
 const initialState = {
-  authenticated: false
+  authenticated: false,
+  loading: false,
+  errors: null
 };
 
 export default function userReducer(state = initialState, action) {
@@ -10,11 +18,28 @@ export default function userReducer(state = initialState, action) {
     case TOKEN:
       newState = Object.assign({}, state, { authenticated: true });
       return newState;
-
+    case SET_USER:
+      return {
+        authenticated: true,
+        loading: false,
+        ...action.payload
+      };
     case LOGOUT:
       localStorage.removeItem("userId");
       localStorage.removeItem("token");
       return state;
+    case SET_ERRORS:
+      return {
+        ...state,
+        loading: false,
+        errors: action.payload
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        loading: false,
+        errors: null
+      };
     default:
       return state;
   }
