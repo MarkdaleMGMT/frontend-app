ec1290d13a45aeaa737b51e933e5de8d48c387ba
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-19 23:59:16 -0400

    added the following reducer types : SET_USER, SET_ERRORS, CLEAR_ERRORS, SET_USER, LOADING_USER, LOADING_UI.

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,14 @@
-export const FETCH_TOKEN = 'FETCH_TOKEN';
-export const TOKEN = 'TOKEN';
-export const TOKEN_FAILED = 'TOKEN_FAILED';
-export const SET_USER_ID = 'SET_USER_ID';
-export const RESET = 'RESET';
-export const LOGOUT = 'LOGOUT';
+//user reducer types
+export const FETCH_TOKEN = "FETCH_TOKEN";
+export const TOKEN = "TOKEN";
+export const TOKEN_FAILED = "TOKEN_FAILED";
+export const SET_USER_ID = "SET_USER_ID";
+export const RESET = "RESET";
+export const LOGOUT = "LOGOUT";
+export const SET_ERRORS = "SET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const SET_USER = "SET_USER";
+export const LOADING_USER = "LOADING_USER";
+
+//UI reducer types
+export const LOADING_UI = "LOADING_UI";
