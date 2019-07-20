c0e50bcf9501c39c0fb8b676d84ad827b61bd1cd
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-20 00:39:45 -0400

    added signup action creator

--- src/actions/signUpActions.js
+++ src/actions/signUpActions.js
@@ -1,17 +1,13 @@
-import {
-  SET_USER,
-  SET_ERRORS,
-  CLEAR_ERRORS,
-  LOADING_UI,
-  LOADING_USER
-} from "../actions/types";
-import axios from "axios";
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS } from "../actions/types";
+import api from "../apis/axios_api";
 export const signupUser = (newUserData, history) => dispatch => {
-  dispatch({ type: LOADING_UI });
-  axios
+  api
     .post("/signup", newUserData)
     .then(res => {
-      dispatch(getUserData());
+      dispatch({
+        type: SET_USER,
+        payload: res.data
+      });
       dispatch({ type: CLEAR_ERRORS });
       history.push("/dashboard");
     })
@@ -21,17 +17,4 @@ export const signupUser = (newUserData, history) => dispatch => {
         payload: err.response.data
       });
     });
-};
-
-export const getUserData = () => dispatch => {
-  dispatch({ type: LOADING_USER });
-  axios
-    .get("/user")
-    .then(res => {
-      dispatch({
-        type: SET_USER,
-        payload: res.data
-      });
-    })
-    .catch(err => console.log(err));
 };
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,23 +1,21 @@
-import {
-    TOKEN,
-    LOGOUT
-} from '../actions/types';
+import { TOKEN, LOGOUT } from "../actions/types";
 
 const initialState = {
-    authenticated: false
+  authenticated: false
 };
 
-export default function userReducer (state = initialState, action) {
-    let newState = state;
-    switch (action.type) {
-        case TOKEN:
-            newState = Object.assign({}, state, {authenticated: true});
-            return newState;
-        case LOGOUT:
-            localStorage.removeItem('userId');
-            localStorage.removeItem('token');
-            return state;
-        default:
-            return state;
-    }
+export default function userReducer(state = initialState, action) {
+  let newState = state;
+  switch (action.type) {
+    case TOKEN:
+      newState = Object.assign({}, state, { authenticated: true });
+      return newState;
+
+    case LOGOUT:
+      localStorage.removeItem("userId");
+      localStorage.removeItem("token");
+      return state;
+    default:
+      return state;
+  }
 }
