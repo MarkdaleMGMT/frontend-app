bddbad361c8f78efd7971d0a1fbf6d30c5ccc211
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-22 17:46:58 -0400

    added auth session management action creators

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,13 +4,17 @@ export const TOKEN = "TOKEN";
 export const TOKEN_FAILED = "TOKEN_FAILED";
 export const SET_USER_ID = "SET_USER_ID";
 export const RESET = "RESET";
-export const LOGOUT = "LOGOUT";
 export const SET_ERRORS = "SET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_USER = "SET_USER";
 export const LOADING_USER = "LOADING_USER";
 export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
 export const RESET_PASSWORD = "RESET_PASSWORD";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_INITIATE_LOGOUT = "AUTH_INITIATE_LOGOUT";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
+export const AUTH_CHECK_TIMEOUT = "AUTH_CHECK_TIMEOUT";
 
 //UI reducer types
 export const LOADING_UI = "LOADING_UI";
