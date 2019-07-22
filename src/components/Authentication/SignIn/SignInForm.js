db4fb4f929f653ddea661c33ed0b79c92a48f36b
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-21 20:49:52 -0400

    added validation support using redux-form for signIn component

---
+++ src/components/Authentication/SignIn/SignInForm.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+
+import { NavLink } from "react-router-dom";
+
+const validate = values => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = "Required";
+  } else if (values.username.length < 4) {
+    errors.username = "Minimum be 4 characters or more";
+  }
+
+  if (!values.password) {
+    errors.password = "Required";
+  } else if (values.password.length < 4) {
+    errors.password = "Minimum be 4 characters or more";
+  }
+
+  return errors;
+};
+
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning }
+}) => (
+  <div>
+    <input
+      {...input}
+      placeholder={label}
+      type={type}
+      className="form-control"
+    />
+    {touched &&
+      ((error && <span className="text-danger">{error}</span>) ||
+        (warning && <span>{warning}</span>))}
+  </div>
+);
+
+class SignIn extends Component {
+  onSubmit = formValues => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    const { errors, handleSubmit, pristine, submitting } = this.props;
+    return (
+      <div className="signin-container">
+        <div>
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <div className="form-group">
+              <Field name="username" component={renderField} label="Username" />
+            </div>
+            <div className="form-group">
+              <Field name="password" component={renderField} label="Password" />
+            </div>
+
+            {/* rendering server side validation */}
+            <p className="text-danger">{errors && errors.msg}</p>
+            <div className="form-group">
+              <button
+                type="submit"
+                disabled={pristine || submitting}
+                className="btn btn-info signin-btn"
+              >
+                SignIn
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className="signup-options-container">
+          <NavLink to="/signIn" className="signup-link">
+            Sign In
+          </NavLink>
+          <NavLink to="/forgotpassword" className="forgot-password-link">
+            Forgot
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "signin",
+  validate
+})(SignIn);
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,55 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchToken } from '../../actions/signInActions';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { fetchToken } from "../../actions/signInActions";
+import SignInForm from "../../components/Authentication/SignIn/SignInForm";
 
-import './SignIn.scss';
+import "./SignIn.scss";
 
 class SignIn extends Component {
-  render(){
-    return (
-      <div className="signin-container">
-        <div >
-            <form>
-                <div className="form-group">
-                    <input type="text" className="form-control" id="userName" placeholder="Username"></input>
-                </div>
-                <div className="form-group">
-                    <input type="password" className="form-control" id="password" placeholder="Password"></input>
-                </div>
-                <div>
-                    <button type="submit" name="signIn" className="btn btn-info signin-btn">Login</button>
-                </div>
-            </form>
-        </div>
-        <div className="signup-options-container">
-            <NavLink to="/signup" className="signup-link">Sign Up</NavLink>
-            <NavLink to="/forgotpassword" className="forgot-password-link">Forgot</NavLink>
-        </div>
-      </div>
-    );
+  onSubmit = formValues => {
+    this.props.signupUser(formValues, this.props.history);
+  };
+  render() {
+    const { errors } = this.props.store;
+    return <SignInForm errors={errors} onSubmit={this.onSubmit} />;
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    UserStore: state.UserStore
-  }
+    store: state.UserStore
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(
-            {
-                fetchToken,
-            },
-            dispatch
-        )
-    }
+  return {
+    actions: bindActionCreators(
+      {
+        fetchToken
+      },
+      dispatch
+    )
+  };
 }
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(SignIn)
+  mapStateToProps,
+  mapDispatchToProps
+)(SignIn);
