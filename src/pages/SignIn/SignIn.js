e4a876886dc88a024d75bc3689182d21644f2f73
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-21 21:37:04 -0400

    added authtenticate user action creator in the signin page

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { fetchToken } from "../../actions/signInActions";
+import { authenticateUser } from "../../actions/signInActions";
 import SignInForm from "../../components/Authentication/SignIn/SignInForm";
 
 import "./SignIn.scss";
 
 class SignIn extends Component {
   onSubmit = formValues => {
-    this.props.signupUser(formValues, this.props.history);
+    this.props.authenticateUser(formValues, this.props.history);
   };
   render() {
     const { errors } = this.props.store;
@@ -26,7 +26,7 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(
       {
-        fetchToken
+        authenticateUser
       },
       dispatch
     )
