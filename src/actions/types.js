3f9e620c6e2caf84536a3b5f763fd55bd4780208
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-21 21:25:10 -0400

    Added SET_AUTHENTICATED action creator type

--- src/actions/types.js
+++ src/actions/types.js
@@ -9,6 +9,7 @@ export const SET_ERRORS = "SET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_USER = "SET_USER";
 export const LOADING_USER = "LOADING_USER";
+export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
 
 //UI reducer types
 export const LOADING_UI = "LOADING_UI";
