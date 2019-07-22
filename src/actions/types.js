d571db4f21f2d55150b5726375ac9778fd215af9
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-21 23:12:03 -0400

    Added RESET_PASSWORD action type

--- src/actions/types.js
+++ src/actions/types.js
@@ -10,6 +10,7 @@ export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_USER = "SET_USER";
 export const LOADING_USER = "LOADING_USER";
 export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
+export const RESET_PASSWORD = "RESET_PASSWORD";
 
 //UI reducer types
 export const LOADING_UI = "LOADING_UI";
