97a51e3069d4e2f2277d013262134b4bd2b3a076
ahana <ahanaghosh94@gmail.com>
2020-02-23 13:18:57 -0500

    fix merge conflicts

--- src/components/ResponsiveSidebar/ResponsiveSidebar.js
+++ src/components/ResponsiveSidebar/ResponsiveSidebar.js
@@ -84,8 +84,4 @@ class ResponsiveSidebar extends Component {
   }
 }
 
-<<<<<<< HEAD
 export default ResponsiveSidebar;
-=======
-export default (ResponsiveSidebar);
->>>>>>> 2eee2f513c5b6fe4b5bfa274b4b80dde3a0407b2
