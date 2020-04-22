6a38519dc3e5ef1721a12f8ec2013d9e50c283fd
ynjacobs <49129946+ynjacobs@users.noreply.github.com>
2020-04-21 22:22:42 -0400

    add serverIP

--- src/pages/Investment/Investment.js
+++ src/pages/Investment/Investment.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { serverIP } from '../../../src/config'
 import {   
     ResponsiveSidebar,
     LeftSidebar,
