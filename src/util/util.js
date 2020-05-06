02638f9ae2ab14343b7358e9fb926a666faa08cb
ynjacobs <49129946+ynjacobs@users.noreply.github.com>
2020-05-06 15:01:14 -0400

    added new function for formatting with no decimals

--- src/util/util.js
+++ src/util/util.js
@@ -2,7 +2,7 @@ export function formatAmount(amount, isCAD=false){
 
   if(!amount) return "-";
 
-  let dp = isCAD? 2 : 8;
+  let dp = isCAD?   0 : 8;
 
   let strAmount = parseFloat(amount).toFixed(dp);
   let [beforeDecimal, afterDecimal] = strAmount.split(".");
@@ -20,7 +20,34 @@ export function formatAmount(amount, isCAD=false){
   // console.log(beforeDecimal)
   // console.log(afterDecimal)
 
-  let formattedAmount = [beforeDecimal, afterDecimal].join(". ")
+  let formattedAmount = [beforeDecimal, afterDecimal].join("  ")
+  formattedAmount = formattedAmount.trim().replace(/(^,)|(,$)/g, "")
+  return formattedAmount.trim();
+}
+
+export function formaterAmount(amount, isCAD=false){
+
+  if(!amount) return "-";
+
+  let dp = isCAD?  2 : 8;
+
+  let strAmount = parseFloat(amount).toFixed(dp);
+  let [beforeDecimal, afterDecimal] = strAmount.split(".");
+  // console.log(beforeDecimal, afterDecimal)
+
+  let reverseBeforeDecimal = beforeDecimal.split("").reverse().join("")
+  beforeDecimal = reverseBeforeDecimal.replace(/.{3}/g, '$& ,').split("").reverse().join("")
+
+  if(!afterDecimal)
+    afterDecimal = ''
+  else
+    afterDecimal = afterDecimal.replace(/.{4}/g, '$&, ')
+
+  //
+  // console.log(beforeDecimal)
+  // console.log(afterDecimal)
+
+  let formattedAmount = [beforeDecimal, afterDecimal].join(" . ")
   formattedAmount = formattedAmount.trim().replace(/(^,)|(,$)/g, "")
   return formattedAmount.trim();
 }
