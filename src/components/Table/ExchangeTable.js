2d1a2200a972d8fa1b20b6ea59cdde3c806c87fd
ynjacobs <49129946+ynjacobs@users.noreply.github.com>
2020-05-07 15:41:00 -0400

    fix for ask

--- src/components/Table/ExchangeTable.js
+++ src/components/Table/ExchangeTable.js
@@ -43,7 +43,7 @@ export default class ExchangeTable extends Component {
             {
                 Header:'Ask',
                 id:'ask',
-                accessor: (data)=> formatAmount(+data.rate_in_cad.ask, true),
+                accessor: (data)=> formaterAmount((+data.rate_in_cad.bid).toFixed(3), true),
                 sortMethod: (a, b) => {
 
                     console.log("cad: ",a,b)
